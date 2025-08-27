11273381c3d48ce33668906117b2d459e107688b
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2025-08-27 07:44:34 -0300

    init: Documentação

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -15,16 +15,16 @@ public class Principal {
 //        long fim = leitor.nextInt();
 
         long inicio = 10;  // Valor inicial do intervalo
-        long fim = 500000000;     // Valor final do intervalo
+        long fim = 500000000;     // 500 milhões. Valor final do intervalo
 
         System.out.println("Números primos do intervalo " + inicio + " e " + fim + ":");
         //Dividindo o intervalo em dois sub-intervalos.
 
-        System.out.println("Segmento 1 entre " + inicio + " e " + ((int) fim / 2 - 1) + ":");
+        System.out.println("Sub-intervalo 1 entre " + inicio + " e " + ((int) fim / 2 - 1) + ":");
         //Inicializando o objeto que calcula os primos
         CalculaPrimo cp1 = new CalculaPrimo(inicio, (int) fim / 2 - 1);
 
-        System.out.println("Segmento 2 entre " + ((int) fim / 2) + " e " + fim + ":");
+        System.out.println("Sub-intervalo 2 entre " + ((int) fim / 2) + " e " + fim + ":");
         //Inicializando o objeto que calcula os primos
         CalculaPrimo cp2 = new CalculaPrimo((int) fim / 2, fim);
 
