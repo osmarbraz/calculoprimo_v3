08e454d206910043aa8a92fed4caba7e66ba0dd4
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2025-08-28 18:08:23 -0300

    fix: iniciando o intervalo em 1

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
 //        System.out.print("Digite o valor final: ");
 //        long fim = leitor.nextInt();
 
-        long inicio = 10;  // Valor inicial do intervalo
+        long inicio = 1;  // Valor inicial do intervalo
         long fim = 500000000;     // 500 milhões. Valor final do intervalo
 
         System.out.println("Números primos do intervalo " + inicio + " e " + fim + ":");
